d8c1882a8b81478ebf37526f2dfa2dc40f7a34e8
GolubevDS <golubev.rocket@gmail.com>
2023-07-25 21:59:49 +0800

    Remove SAV token whitelist mode and remove SAVR token whitelist mode disable

--- src/components/AdminPanel/blocks/TokenControl.tsx
+++ src/components/AdminPanel/blocks/TokenControl.tsx
@@ -15,15 +15,12 @@ type TokenControlProps = {
 export const TokenControl: FC<TokenControlProps> = ({ token }) => {
   const {
     isPaused,
-    isWhitelistRestrictionMode,
     pause,
     unpause,
     addToBlacklist,
     addToWhitelist,
     removeFromBlacklist,
     removeFromWhitelist,
-    enableWhitelistMode,
-    disableWhitelistMode,
   } = useTokenControl(token);
 
   const isSav = token === ContractsEnum.SAV;
@@ -37,86 +34,56 @@ export const TokenControl: FC<TokenControlProps> = ({ token }) => {
           {isPaused.data ? 'Paused' : 'Active'}
         </Text>
       ) : null}
-      {isWhitelistRestrictionMode.isFetched ? (
-        <Text
-          ml="12px"
-          textStyle="button"
-          color={isWhitelistRestrictionMode.data ? 'red' : 'green.400'}
-        >
-          {isWhitelistRestrictionMode.data ? 'Whitelist' : 'Blacklist'}
-        </Text>
-      ) : null}
     </Flex>
   );
 
   return (
     <AdminSection title={title} isLoading={isPaused.isLoading}>
-      <ButtonGroup size="sm" my="12px" display="block">
-        <Button
-          borderRadius="sm"
-          isDisabled={!isPaused.data || unpause.isLoading || isPaused.isLoading}
-          isLoading={unpause.isLoading}
-          onClick={() => unpause.mutate()}
-        >
-          Unpause
-        </Button>
-        <Button
-          variant="filledRed"
-          isDisabled={isPaused.data || pause.isLoading || isPaused.isLoading}
-          isLoading={pause.isLoading}
-          onClick={() => pause.mutate()}
-        >
-          Pause
-        </Button>
-      </ButtonGroup>
+      <>
+        <ButtonGroup size="sm" my="12px" display="block">
+          <Button
+            borderRadius="sm"
+            isDisabled={!isPaused.data || unpause.isLoading || isPaused.isLoading}
+            isLoading={unpause.isLoading}
+            onClick={() => unpause.mutate()}
+          >
+            Unpause
+          </Button>
+          <Button
+            variant="filledRed"
+            isDisabled={isPaused.data || pause.isLoading || isPaused.isLoading}
+            isLoading={pause.isLoading}
+            onClick={() => pause.mutate()}
+          >
+            Pause
+          </Button>
+        </ButtonGroup>
 
-      <ButtonGroup size="sm" mb="12px">
-        <Button
-          borderRadius="sm"
-          isDisabled={
-            !isWhitelistRestrictionMode.data ||
-            disableWhitelistMode.isLoading ||
-            isWhitelistRestrictionMode.isLoading
-          }
-          isLoading={disableWhitelistMode.isLoading}
-          onClick={() => disableWhitelistMode.mutate()}
-        >
-          Blacklist only
-        </Button>
-        <Button
-          variant="filledRed"
-          isDisabled={
-            isWhitelistRestrictionMode.data ||
-            enableWhitelistMode.isLoading ||
-            isWhitelistRestrictionMode.isLoading
-          }
-          isLoading={enableWhitelistMode.isLoading}
-          onClick={() => enableWhitelistMode.mutate()}
-        >
-          Enable Whitelist
-        </Button>
-      </ButtonGroup>
-
-      <ControlField
-        label="Add to blacklist"
-        onSubmit={addToBlacklist.mutateAsync}
-        tip="Split addresses with comma"
-      />
-      <ControlField
-        label="Remove from blacklist"
-        onSubmit={removeFromBlacklist.mutateAsync}
-        tip="Split addresses with comma"
-      />
-      <ControlField
-        label="Add to whitelist"
-        onSubmit={addToWhitelist.mutateAsync}
-        tip="Split addresses with comma"
-      />
-      <ControlField
-        label="Remove from whitelist"
-        onSubmit={removeFromWhitelist.mutateAsync}
-        tip="Split addresses with comma"
-      />
+        <ControlField
+          label="Add to blacklist"
+          onSubmit={addToBlacklist.mutateAsync}
+          tip="Split addresses with comma"
+        />
+        <ControlField
+          label="Remove from blacklist"
+          onSubmit={removeFromBlacklist.mutateAsync}
+          tip="Split addresses with comma"
+        />
+        {!isSav ? (
+          <>
+            <ControlField
+              label="Add to whitelist"
+              onSubmit={addToWhitelist.mutateAsync}
+              tip="Split addresses with comma"
+            />
+            <ControlField
+              label="Remove from whitelist"
+              onSubmit={removeFromWhitelist.mutateAsync}
+              tip="Split addresses with comma"
+            />
+          </>
+        ) : null}
+      </>
     </AdminSection>
   );
 };
