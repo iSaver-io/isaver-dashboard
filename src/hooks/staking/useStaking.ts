1daa142ac333091ddb076cec917b4ae3cb89d079
nikitaGetman <nikita.getman56@gmail.com>
2024-03-19 10:44:59 +0300

    Update WithdrawAll success message

--- src/hooks/staking/useStaking.ts
+++ src/hooks/staking/useStaking.ts
@@ -148,34 +148,41 @@ export const useActiveStakingPlansWithUserInfo = () => {
               (s) => s.data?.[0]?.stakingPlanId === plan.stakingPlanId
             )?.data;
 
-            const { totalReward, totalDeposit, totalAvailableReward } = stakes
-              ? stakes.reduce(
-                  (acc, stake) => {
-                    if (stake.isClaimed) {
+            const { totalReward, totalDeposit, totalAvailableReward, totalAvailableDeposit } =
+              stakes
+                ? stakes.reduce(
+                    (acc, stake) => {
+                      if (stake.isClaimed) {
+                        return acc;
+                      }
+
+                      if (!stake.isToken2) {
+                        acc.totalDeposit = acc.totalDeposit.add(stake.amount);
+                      }
+                      acc.totalReward = acc.totalReward.add(stake.profit);
+
+                      if (stake.timeEnd.toNumber() < Date.now() / 1000) {
+                        acc.totalAvailableReward = acc.totalAvailableReward.add(stake.profit);
+                        if (!stake.isToken2) {
+                          acc.totalAvailableDeposit = acc.totalAvailableDeposit.add(stake.amount);
+                        }
+                      }
+
                       return acc;
+                    },
+                    {
+                      totalReward: BigNumber.from(0),
+                      totalDeposit: BigNumber.from(0),
+                      totalAvailableReward: BigNumber.from(0),
+                      totalAvailableDeposit: BigNumber.from(0),
                     }
-                    if (!stake.isToken2) {
-                      acc.totalDeposit = acc.totalDeposit.add(stake.amount);
-                    }
-                    acc.totalReward = acc.totalReward.add(stake.profit);
-
-                    if (stake.timeEnd.toNumber() < Date.now() / 1000) {
-                      acc.totalAvailableReward = stake.profit;
-                    }
-
-                    return acc;
-                  },
-                  {
-                    totalReward: BigNumber.from(0),
-                    totalAvailableReward: BigNumber.from(0),
-                    totalDeposit: BigNumber.from(0),
-                  }
-                )
-              : {
-                  totalReward: undefined,
-                  totalDeposit: undefined,
-                  totalAvailableReward: undefined,
-                };
+                  )
+                : {
+                    totalReward: undefined,
+                    totalDeposit: undefined,
+                    totalAvailableReward: undefined,
+                    totalAvailableDeposit: undefined,
+                  };
 
             const hasReadyStakes = stakes?.some(
               (stake) => stake.timeEnd.toNumber() <= currentTime && !stake.isClaimed
@@ -186,8 +193,9 @@ export const useActiveStakingPlansWithUserInfo = () => {
               ...userPlansInfoRequest.data?.[index],
               isSubscriptionEnding,
               totalReward,
-              totalAvailableReward,
               totalDeposit,
+              totalAvailableReward,
+              totalAvailableDeposit,
               stakes,
               hasReadyStakes,
             };
@@ -363,7 +371,7 @@ export const useStakingActions = () => {
       success({
         title: 'Success',
         description: getWithdrawMessage(
-          stakingPlan?.totalDeposit,
+          stakingPlan?.totalAvailableDeposit,
           stakingPlan?.totalAvailableReward
         ),
         txHash,
